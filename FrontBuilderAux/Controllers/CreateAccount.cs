1cb3b7ae769aeb88cd9f4574d1f5aec639fa7e9f
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2023-10-16 17:04:56 -0300

    odeio front

--- FrontBuilderAux/Controllers/CreateAccount.cs
+++ FrontBuilderAux/Controllers/CreateAccount.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FrontBuilderAux.Models;
+using FrontBuilderAux.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FrontBuilderAux.Controllers
 {
     public class CreateAccount : Controller
     {
+        private readonly IBuilderAuxGateWayService _gateWay;
+        public CreateAccount(IBuilderAuxGateWayService gateWay)
+        {
+            _gateWay = gateWay ?? throw new ArgumentNullException(nameof(gateWay));
+        }
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception er)
+            {
+                TempData["MensagemErro"] = $"opa! Deu merda, hein: {er.Message}";
+                return RedirectToAction("Index");
+
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> CriarContaAsync(Usuarios user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _ = await _gateWay.PostAsync(user);
+                }
+
+                var mensagensErro = ModelState.Values.SelectMany(v => v.Errors)
+                                       .Select(e => e.ErrorMessage)
+                                       .ToList();
+                TempData["MessageErro"] = mensagensErro;
+                return View("Index");
+            }
+            catch (Exception er)
+            {
+                TempData["MensagemErro"] = $"opa! Deu merda, hein: {er.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
--- FrontBuilderAux/Models/Usuarios.cs
+++ FrontBuilderAux/Models/Usuarios.cs
@@ -1,24 +1,33 @@
-﻿namespace FrontBuilderAux.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Security;
+
+namespace FrontBuilderAux.Models
 {
     public class Usuarios
     {
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string? Role { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string? Senha { get; set; }
+        [Compare("Senha", ErrorMessage = "As senhas não coincidem")]
+        public string? ConfirmarSenha { get; set; }
 
 
-        public Usuarios(string name, string email)
+        public Usuarios()
         {
-            Name = name;
-            Email = email;
+            
         }
-        public Usuarios(string name, string email, string role, string senha)
+        public Usuarios(string name, string email, string? role, string? senha, string? confirmarSenha)
         {
-            Name = name;
-            Email = email;
-            Role = role;
-            Senha = senha;
+            Name = name ?? throw new ArgumentNullException();
+            Email = email ?? throw new ArgumentNullException();
+            Role = role ?? throw new ArgumentNullException();
+            Senha = senha ?? throw new ArgumentNullException();
+            ConfirmarSenha = confirmarSenha ?? throw new ArgumentNullException();
         }
 
 
